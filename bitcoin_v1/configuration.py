fb68357a48a2e03ae0d5e1511b0d115ecb5e2db5
DJ <DJ@devenv-NS>
2014-04-07 20:49:52 -0400

    Update COnfig

--- bitcoin_v1/configuration.py
+++ bitcoin_v1/configuration.py
@@ -36,13 +36,13 @@ else:
     NETWORK_PORT = TEST_NETWORK_PORT
     NETWORK_ADDRESS = TESTNET3_PEER_HOST_IP
 
-PRIVATE_KEY_LIST = ["f19c523315891e6e15ae0608a35eec2e00ebd6d1984cf167f46336dabd9b2de4",
+PRIVATE_KEY_LIST = ["d3484a00246dee175bfc092b7ed7bb3067ec78083035d9cfd34cec06f40c4cc2",
                     #"f19c523315891e6e15ae0608a35eec2e00ebd6d1984cf167f46336dabd9b2de4", 
                     #"d3484a00246dee175bfc092b7ed7bb3067ec78083035d9cfd34cec06f40c4cc2"
                     ]
 
 # The transaction hash of the previous 
-PREVIOUS_TRANSACTION_HASH_LIST = ["55fe5f8599add2a1fb91a104d16a67fba91d8f3fc24b9e2353b216ed4c015140", #4.99
+PREVIOUS_TRANSACTION_HASH_LIST = ["1df492ae7b95d6b1fe432a2c5ebf0acba8b5c861653bfc2f9ef6663bf239ddcc", #0.40
                                   #"a94b239a16e923df5e4a447062f9c71a9d5cfb765086d593caf41aa01abb2441",
                                   ] #.6.9
 
@@ -51,17 +51,17 @@ PREVIOUS_TRANSACTION_HASH_LIST = ["55fe5f8599add2a1fb91a104d16a67fba91d8f3fc24b9
 #     - This indicates which of the outputs in the previous transaction
 #       to send money from.
 #     - 0: First, 1: Second, ...
-PREVIOUS_TRANSACTION_OUTPUT_INDEX_LIST = [0,
+PREVIOUS_TRANSACTION_OUTPUT_INDEX_LIST = [1,
                                           #0
                                           ]
 
-PREVIOUS_TRANSACTION_OUTPUT_PUBLIC_ADDRESS_LIST = ["myqGc9SzpYm3qFU83UNhMZ8mjCFqzmGi2x"
+PREVIOUS_TRANSACTION_OUTPUT_PUBLIC_ADDRESS_LIST = ["mtBRkCj8GSc5kPYqhLdS4ahaTMXfk5trq1"
                                                    ]
 
 
 # BTC Denomination <=> Satoshis: 100,000,000 -> 1 BTC
 #     - 0.40 BTC
-DESTINATION_SATOSHIS_LIST = [499000000]
+DESTINATION_SATOSHIS_LIST = [39000000]
 
 DESTINATION_PUBLIC_ADDRESS_LIST = ["mjiu4Jwr48SzMhEvT6aaiNwPekPksXNRmf"]
 
